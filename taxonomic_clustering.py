19c212b8adf0ae728c7c7b86fc3a35a8680413b5
Bob Costas <finkzane@gmail.com>
2018-07-02 15:23:47 -0700

    Added command-line option to include a file of taxonomic lineage

--- taxonomic_clustering.py
+++ taxonomic_clustering.py
@@ -18,11 +18,14 @@ def main():
     if options.query is None:
         print( "Fasta query file must be provided." )
         sys.exit() 
-
+    elif options.tax is None:
+        print( "Taxonomic lineage file must be provided." )
+        sys.exit() 
 
 
 def add_program_options( option_parser ):
     option_parser.add_option( '-q', '--query', help = "Fasta query file to read sequences from and do ordering of. [None, Required]" )
+    option_parser.add_option( '-t', '--tax', help = "Taxonomic lineage file such as the one from ftp://ftp.ncbi.nlm.nih.gov/pub/taxonomy/" )
 
 if __name__ == '__main__':
     main()
