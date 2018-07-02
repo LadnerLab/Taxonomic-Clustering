dc36fbec1145ddf79f89e33e2677c3f0fe6dddaf
Bob Costas <finkzane@gmail.com>
2018-07-02 15:34:55 -0700

    Added start and number options to program_options

--- taxonomic_clustering.py
+++ taxonomic_clustering.py
@@ -22,10 +22,17 @@ def main():
         print( "Taxonomic lineage file must be provided." )
         sys.exit() 
 
+    names, sequences = oligo.read_fasta_lists( options.query )
 
 def add_program_options( option_parser ):
     option_parser.add_option( '-q', '--query', help = "Fasta query file to read sequences from and do ordering of. [None, Required]" )
     option_parser.add_option( '-t', '--tax', help = "Taxonomic lineage file such as the one from ftp://ftp.ncbi.nlm.nih.gov/pub/taxonomy/" )
+    option_parser.add_option( '-n', '--number', type = int, default = 10000,
+                              help = "Threshold value for determining cutoff of number of sequences that can be included in output. [10,000]"
+                            )
+    option_parser.add_option( '-s', '--start', default = 'family',
+                              help = "Level of the taxonomic hierarchy at which to begin clustering. [family]"
+                            )
 
 if __name__ == '__main__':
     main()
