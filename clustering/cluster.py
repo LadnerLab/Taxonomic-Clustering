4111a7a208d362cd684c0716732fbf6a2b73b63a
Bob Costas <finkzane@gmail.com>
2018-08-14 09:23:58 -0700

    added cluster and its subclasses

---
+++ clustering/cluster.py
@@ -0,0 +1,24 @@
+
+
+class Cluster:
+    def __init__( self ):
+        self.names = None
+        self.sequence = None
+        self.kmer_dict = {}
+        self.sequence_dict = {}
+
+
+
+
+
+
+        
+
+class TaxonomicCluster( Cluster ):
+    def __init__( self ):
+        super().__init( self )__
+
+
+class KmerCluster( Cluster ):
+    def __init__( self ):
+        super().__init( self )__
